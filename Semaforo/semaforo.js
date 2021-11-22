88d1f578be68b836872177d463cc19d080522a02
VinicinBraga <vinicius@bragamatta.com.br>
2021-11-22 11:52:23 -0300

    Controlando o setInterval

--- Semaforo/semaforo.js
+++ Semaforo/semaforo.js
@@ -1,8 +1,10 @@
 const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 let colorIndex = 0;
+let intervalId = null;
 
 const trafficLight = (e) => {
+  stopInterval();
   turnON[e.target.id]();
 };
 
@@ -17,11 +19,15 @@ nextIndex = () => {
   colorIndex = colorIndex < 2 ? ++colorIndex : 0;
 };
 
+stopInterval = () => {
+  clearInterval(intervalId);
+};
+
 const turnON = {
   red: () => (img.src = "./img/vermelho.png"),
   yellow: () => (img.src = "./img/amarelo.png"),
   green: () => (img.src = "./img/verde.png"),
-  automatic: () => setInterval(aleatoryChange, 1000),
+  automatic: () => (intervalId = setInterval(aleatoryChange, 1000)),
 };
 
 buttons.addEventListener("click", trafficLight);
