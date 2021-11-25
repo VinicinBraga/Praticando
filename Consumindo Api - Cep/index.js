f36ac53684baa11e682bf8fa65dccf79385ac311
VinicinBraga <vinicius@bragamatta.com.br>
2021-11-25 20:37:23 -0300

    Implementando tratamento de erro

--- Consumindo Api - Cep/index.js
+++ Consumindo Api - Cep/index.js
@@ -1,26 +1,43 @@
 "use strict";
 
+const clearForm = () => {
+  document.getElementById("endereco").value = " ";
+  document.getElementById("bairro").value = " ";
+  document.getElementById("cidade").value = " ";
+  document.getElementById("estado").value = " ";
+};
+
 const fillForm = (addressParam) => {
   document.getElementById("endereco").value = addressParam.logradouro;
   document.getElementById("bairro").value = addressParam.bairro;
   document.getElementById("cidade").value = addressParam.localidade;
   document.getElementById("estado").value = addressParam.uf;
 };
+const isNumber = (numb) => /^[0-9]+$/.test(numb);
+const cepValidation = (cepParam) => cepParam.length === 8 && isNumber(cepParam);
 
 const searchCep = async () => {
   const cep = document.getElementById("cep").value;
   const url = `http://viacep.com.br/ws/${cep}/json/`;
-  /*fetch(url)
-    .then((response) => response.json())
-    .then(console.log);*/
-  const data = await fetch(url);
-  const address = await data.json();
-  console.log(address);
-  if (address.hasOwnProperty("erro")) {
-    alert(`O cep ${cep} não foi encontrado`);
+
+  if (cepValidation(cep)) {
+    const data = await fetch(url);
+    const address = await data.json();
+    console.log(address);
+    if (address.hasOwnProperty("erro")) {
+      clearForm();
+      document.getElementById("endereco").value = `CEP não encontrado`;
+    } else {
+      fillForm(address);
+    }
   } else {
-    fillForm(address);
+    clearForm();
+    document.getElementById("endereco").value = `Formato inválido`;
   }
 };
 
 document.getElementById("cep").addEventListener("focusout", searchCep);
+
+/*fetch(url)
+    .then((response) => response.json())
+    .then(console.log);*/
