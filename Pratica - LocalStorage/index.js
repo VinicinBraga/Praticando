c473961cb5dc85c23be396001a4ad0fc7572eedf
VinicinBraga <vinicius@bragamatta.com.br>
2021-11-26 10:39:28 -0300

    Exercicio LocalStorage

---
+++ Pratica - LocalStorage/index.js
@@ -0,0 +1,18 @@
+document.getElementById("estado").value = localStorage.estado;
+document.getElementById("dataInicio").value = localStorage.inicio;
+document.getElementById("dataFinal").value = localStorage.final;
+
+const saveData = () => {
+  const state = document.getElementById("estado").value;
+  const initialDate = document.getElementById("dataInicio").value;
+  const finalDate = document.getElementById("dataFinal").value;
+  const obj = {
+    state,
+    initialDate,
+    finalDate,
+  };
+  console.log(obj);
+  localStorage.setItem("estado", obj.state);
+  localStorage.setItem("inicio", obj.initialDate);
+  localStorage.setItem("final", obj.finalDate);
+};
