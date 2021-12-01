22ffc33868c75a194d29bfacc86a6e47fdc7f45e
VinicinBraga <vinicius@bragamatta.com.br>
2021-12-01 15:28:34 -0300

    Atualizando LocalStorage

---
+++ Pratica - LocalStorage/index.js
@@ -0,0 +1,33 @@
+document.getElementById("name").value = localStorage.name;
+document.getElementById("surname").value = localStorage.surname;
+document.getElementById("email").value = localStorage.email;
+document.getElementById("cell").value = localStorage.cell;
+document.getElementById("city").value = localStorage.city;
+document.getElementById("birth").value = localStorage.birth;
+document.getElementById("state").value = localStorage.state;
+
+const saveData = () => {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("email").value;
+  const cell = document.getElementById("cell").value;
+  const birth = document.getElementById("birth").value;
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+  const obj = {
+    name,
+    surname,
+    email,
+    cell,
+    birth,
+    city,
+    state,
+  };
+  localStorage.setItem("name", obj.name);
+  localStorage.setItem("surname", obj.surname);
+  localStorage.setItem("email", obj.email);
+  localStorage.setItem("cell", obj.cell);
+  localStorage.setItem("birth", obj.birth);
+  localStorage.setItem("city", obj.city);
+  localStorage.setItem("state", obj.state);
+};
