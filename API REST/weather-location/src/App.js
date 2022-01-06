d32582e74481ab937788a92823b9da028a322040
VinicinBraga <vinicius@bragamatta.com.br>
2022-01-06 16:33:33 -0300

    Estrutura aplicaçao - Clima

---
+++ API REST/weather-location/src/App.js
@@ -0,0 +1,59 @@
+import { Fragment, useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, lon) => {
+    let res = await axios.get(
+      "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}",
+      {
+        params: {
+          lat: lat,
+          lon: lon,
+          appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        },
+      }
+    );
+    setWeather(res.data);
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  if (location === false) {
+    return (
+      <Fragment>
+        É necessário permissão de acesso da localização no seu browser
+      </Fragment>
+    );
+  } else if (weather === false) {
+    return <Fragment>Carregando Clima...</Fragment>;
+  } else {
+    return (
+      <Fragment>
+        <h3>
+          Clima nas suas Coordenadas ({weather["main"][0]["description"]})
+        </h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: {weather["main"]["temp"]}º</li>
+          <li>Temperatura máxima:{weather["main"]["temp_max"]}º</li>
+          <li>Temperatura minima: {weather["main"]["temp_min"]}º</li>
+          <li>Pressão: {weather["main"]["pressure"]} hpa</li>
+          <li>Umidade: {weather["main"]["humidity"]}%</li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
