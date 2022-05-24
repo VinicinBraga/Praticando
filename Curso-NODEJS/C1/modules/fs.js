b6744799f552f90680fe042f41fa176e915007d5
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-24 17:20:15 -0300

    Incluindo conteudo no arquivo txt

--- Curso-NODEJS/C1/modules/fs.js
+++ Curso-NODEJS/C1/modules/fs.js
@@ -2,21 +2,32 @@ const fs = require("fs");
 const path = require("path");
 
 //Criar Pasta
-fs.mkdir(path.join(__dirname, "/test"), (error) => {
+/* fs.mkdir(path.join(__dirname, "/test"), (error) => {
   if (error) {
     return console.log("Erro: ", error);
   }
   console.log("pasta criada com sucesso");
-});
+}); */
 
 //Criar Arquivo
-fs.writeFile(
+/* fs.writeFile(
   path.join(__dirname, "/test", "test.txt"),
   "Hello Node!",
   (error) => {
     if (error) {
       return console.log("Erro: ", error);
     }
-    console.log("pasta e arquivo criados com sucesso");
+    console.log("Pasta e arquivo criados com sucesso");
+  }
+); */
+
+fs.appendFile(
+  path.join(__dirname, "/test", "test.txt"),
+  " Hello Everyone!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+    console.log("Conteúdo Incluido no arquivo de texto");
   }
 );
