8b575103bd30008db0db47fd282ab2c22764bed0
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-24 17:16:24 -0300

    Criando diretorio e arquivo no diretorio

--- Curso-NODEJS/C1/modules/fs.js
+++ Curso-NODEJS/C1/modules/fs.js
@@ -8,3 +8,15 @@ fs.mkdir(path.join(__dirname, "/test"), (error) => {
   }
   console.log("pasta criada com sucesso");
 });
+
+//Criar Arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "Hello Node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+    console.log("pasta e arquivo criados com sucesso");
+  }
+);
