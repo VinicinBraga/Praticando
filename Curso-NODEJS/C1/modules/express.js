7ef703e4d46fea5aefc35b757f93936f573a6086
VinicinBraga <vinicius@bragamatta.com.br>
2022-06-28 11:53:36 -0300

    POST and GET

--- Curso-NODEJS/C1/modules/express.js
+++ Curso-NODEJS/C1/modules/express.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const { Person } = require("../person");
+const UserModel = require("../src/models/user.model");
+
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+
 const person = new Person("Vinicius");
 
 app.get("/home", (req, res) => res.status(200).send(`Hi, ${person.name}!!`));
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Thiago Neves",
-      email: "Tneves@zero.com",
-    },
-    {
-      name: "Zezé Perrela",
-      email: "zeze@perrela.com",
-    },
-  ];
-  res.status(200).json(users);
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
